a4138d463b868527b02077a2a0f565db1044048e
Paresh Bharambe <pareshb@live.in>
2018-12-06 16:57:57 +0530

    Solved production database connection url

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 
+const app = require('express')();
+
 mongoose.Promise = global.Promise;
-let url = "mongodb://localhost:27017/TodoApp";
+
+if(app.settings.env == 'development') {
+    let url = "mongodb://localhost:27017/TodoApp";
+} else {
+    let url = "mongodb+srv://Watson:KZsSECt4123@cluster0-d3zkk.mongodb.net/test?retryWrites=true";
+}
+
 
 mongoose.connect(url, {useNewUrlParser:true}).then(()=>{
 
